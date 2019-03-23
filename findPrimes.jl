f3a75c4395e0f66aa640f41080eaa6b98505c350
Vincent Feltkamp <vincy@sapo.cv>
2019-03-23 07:36:46 +0100

    moltoamico added and computed up to 4th degree

--- findPrimes.jl
+++ findPrimes.jl
@@ -4,18 +4,19 @@ function findFactorsIn(n=(2),potFactors=[2,3],nr=0,startindex=1, compositeFactor
   factors = BigInt[]
   if n==1 return factors end
   for p in potFactors[startindex:end]
-   if n%p==0
-     if debug>2 print(p, " ") end
-     push!(factors,p)
-     if nr==1 return factors end
-     if !compositeFactors
-        append!(factors,findFactorsIn(div(n,p), potFactors, nr-1,indexin(p,primes)[1],false, debug))
-        return factors
+    if p<=n && n%p==0
+      if debug>2 print(p, " ") end
+      push!(factors,p)
+      if nr==1 return factors end
+      if !compositeFactors
+            append!(factors,findFactorsIn(div(n,p), potFactors, nr-1,indexin(p,primes)[1],false, debug))
+      return factors
       end
     end
-   end
+  end
   return factors
 end
+
 function findDistinctDivisors(n,debug=0)
   return findFactorsIn(n,2:(n-1),0,1,true, debug)
 end
@@ -24,14 +25,14 @@ function findAFactor(n=2,primes=[2,3])
   return findFactorsIn(n,primes,1,1,false)
 end
 
-function findPrimeFactors!(n=2,primes=[2,3],nr=0)
+function findPrimeFactors!(n=2,primes=[2,3],nr=0,start=1)
   factors = BigInt[]
-  for p in primes
-    if n%p==0
+  for p in primes[start:end]
+    if n>=p && n%p==0
       push!(factors,p)
       if nr!=1 && n!=p
         #println("factorizing $n finding $nr factors ")
-        append!(factors,findPrimeFactors!(div(n,p), primes, nr-1))
+        append!(factors,findPrimeFactors!(div(n,p), primes, nr-1,indexin(p,primes)[1]))
       end
       break
     end
@@ -39,7 +40,7 @@ function findPrimeFactors!(n=2,primes=[2,3],nr=0)
   if length(factors)==0
      if primes[length(primes)]< n
        AppendNPrimes!(1,primes)
-       append!(factors,findPrimeFactors!(n,primes,nr))
+       append!(factors,findPrimeFactors!(n,primes,nr,length(primes)))
      else println("No factors found, largest factor checked is ",primes[length(primes)])
        return factors
      end
@@ -76,42 +77,6 @@ function makeDivisorswithMultiplicity(n, primes=primes, debug=0)
   return (divisorswithMultiplicity)
 end
 
-## unittests
-@assert(findDistinctDivisors((28),3)==[2,4,7,14])
-primes=BigInt[2,3]
-@assert findFactorsIn(28,primes,0,1, false,3)==[2,2] #7 not in primes yet
-@assert findPrimeFactors!(28,primes)==[2,2,7] # this function adds new primes
-@assert findFactorsIn(28,primes,0,1, false,3)==[2,2,7]
-@assert findFactorsIn(28,primes,0,1, true,3)==[2,7]
-@assert findAFactor(28,primes)== [2]
-biggie=30*1001*17
-@assert findDistinctPrimeFactors(510510^2)==[2,3,5,7]#
-@assert findPrimeFactors!(17017,primes)==[7,11,13,17]  #adds 11,13,17 to primes
-@assert findDistinctPrimeFactors(biggie^2)==[2,3,5,7,11,13,17]
-@assert findmultiplePrimeFactors((biggie+1)^2)==BigInt[]  #19,19,19,97,97,97,277,277,277 not yet found
-@assert findPrimeFactors!(biggie+1,primes)==[19,97,277]  #adds primes up to 277
-@assert findmultiplePrimeFactors((biggie+1)^2)==BigInt[19,19,97,97,277,277] #found
-
-#### now do some timing
-
-@time(findPrimeFactors!(biggie,primes,0))
-@time(findDistinctPrimeFactors(biggie+1,primes))
-@time(findDistinctDivisors(biggie+1))
-@time(findDistinctDivisors(biggie+1))
-print(findPrimeFactors!(biggie+1,primes))
-print(primes')
-@time(findPrimeFactors!(biggie,primes,0))
-@time(findFactorsIn(biggie,2:biggie,0))
-@time(findFactorsIn(biggie+1,2:biggie,0))
-@time(AppendNPrimes!(24,primes))
-@time(AppendNPrimes!(25,primes))
-@time(AppendNPrimes!(25,primes))
-@time(AppendNPrimes!(25,primes))
-@time( AppendNPrimes!(200,primes))
-print(primes[1:50]')
-print(primes')
-
-##
 function sumDivisors(n,debug=0)
   return 1+sum(findDistinctDivisors(n))
 end
@@ -129,7 +94,7 @@ end
 function sumDivisorsMultiply(n,debug=0)
   return sum(makeDivisorswithMultiplicity(n))-n
 end
-function isFatPerfect(n, debug=0)
+function isVeryPerfect(n, debug=0)
   a= sumDivisorsMultiply(n,debug)
   if isodd(debug) print(n," ") end
   if  debug >= 2
@@ -140,40 +105,98 @@ function isFatPerfect(n, debug=0)
   end
   return sign(a-n)
 end
+
 function amico(n,cyclelength=2, debug=0)
   a=n
-  for k = 1:cyclelength a=sumDivisors(a,debug) end
-  return n==sumDivisors(a) ? a : 0
+  for k = 2:cyclelength a=sumDivisors(a,debug) end
+  return n==sumDivisors(a,debug) ? a : 0
 end
+function moltoamico(n,cyclelength,debug=0)
+  a=n
+  for k=2:cyclelength a=sumDivisorsMultiply(a,debug) end
+  return n==sumDivisorsMultiply(a) ? a : 0
+end
+# unittests
+@assert(findDistinctDivisors((28),3)==[2,4,7,14])
+primes=BigInt[2,3]
+@assert findFactorsIn(28,primes,0,1, false,3)==[2,2] #7 not in primes yet
+@assert findPrimeFactors!(28,primes)==[2,2,7] # this function adds new primes
+@assert findFactorsIn(28,primes,0,1, false,3)==[2,2,7]
+@assert findFactorsIn(28,primes,0,1, true,3)==[2,7]
+@assert findAFactor(28,primes)== [2]
+biggie=30*1001*17
+@assert findDistinctPrimeFactors(510510^2)==[2,3,5,7]#
+@assert findPrimeFactors!(17017,primes)==[7,11,13,17]  #adds 11,13,17 to primes
+@assert findDistinctPrimeFactors(biggie^2)==[2,3,5,7,11,13,17]
+@assert findmultiplePrimeFactors((biggie+1)^2)==BigInt[]  #19,19,19,97,97,97,277,277,277 not yet found
+@assert findPrimeFactors!(biggie+1,primes)==[19,97,277]  #adds primes up to 277
+@assert findmultiplePrimeFactors((biggie+1)^2)==BigInt[19,19,97,97,277,277] #found
+## now do some timing
+@time(findPrimeFactors!(biggie,primes,0))
+@time(findDistinctPrimeFactors(biggie+1,primes))
+@time(findDistinctDivisors(biggie+1))
+@time(findDistinctDivisors(biggie+1))
+print(findPrimeFactors!(biggie+1,primes))
+print(primes')
+@time(findPrimeFactors!(biggie,primes,0))
+@time(findFactorsIn(biggie,2:biggie,0))
+@time(findFactorsIn(biggie+1,2:biggie,0))
+@time(AppendNPrimes!(24,primes))
+@time(AppendNPrimes!(25,primes))
+@time(AppendNPrimes!(25,primes))
+@time(AppendNPrimes!(25,primes))
+@time( AppendNPrimes!(200,primes))
+print(primes')
 ## test perfection
-
-isPerfect(28,2)
-isPerfect(29,2)
-@time(begin n=100; print("Perfect numbers until ",n,": ");for i in 1:n isPerfect(i,2) end end)
+@assert isVeryPerfect(6)==0
+@assert isPerfect(28)==0
+@assert isPerfect(29,2)==-1
+@time(begin n=100; print("Perfect numbers until ",n,": ");for i in 1:n isPerfect(i,3) end end)
+# [1,6,28,496,8128]
+@time(@assert begin n=1000; print("Perfect numbers until ",n,": ");
+     [i for i in 1:n if isPerfect(i,0)==0]  end ==[1,6,28,496])
+     # 0.141507 seconds (141.29 k allocations: 5.286 MiB)
 primes=[2,3]
 AppendNPrimes!(100,primes)
-makeDivisorswithMultiplicity(28)==[1,2,2,4,7,14,14,28]
+@assert makeDivisorswithMultiplicity(28)==[1,2,2,4,7,14,14,28]
 for i in 2:10
   list= makeDivisorswithMultiplicity(i)
   println(length(list)," divisors of ",i,": ", list )
 end
-@time(begin n=100; print("Perfect numbers until ",n,": ");for i in 1:n isFatPerfect(i,2) end end)
-
-@time(begin n=100; print("Fat Perfect numbers until ",n,": ");[ isFatPerfect(i,2) for i in 1:n] end)
-@time(  for i in 1:1001 if isFatPerfect(i)==0 println(i, " is Fat perfect")end end)
+@time(@assert begin n=1000; print("very Perfect numbers until ",n,": ");[i for i in 1:n if isVeryPerfect(i,2)==0] end==[6])
+#0.468230 seconds (226.86 k allocations: 77.772 MiB, 7.55% gc time)
+@time(begin n=100; print("verysumdivisors numbers until ",n,": ");[ isVeryPerfect(i,0) for i in 1:n] end)
+@time(  for i in 1:1001 if isVeryPerfect(i)==0 println(i, " is very perfect")end end)
 # until 100001 was 145.212405 seconds (35.06 M allocations: 114.396 GiB, 11.57% gc time)
-# only 6 is fatperfect below 100001
-@time(  for n in 1:1001 a=amico(n);if a!==0  println(n, " has as friend ", a)end end)
-@time(  for n in 1:10001 a=amico(n,3);if a!==0  println(n, " has as 2nd friend ", a)end end)
-@time(  for n in 1:1001 a=amico(n,4);if a!==0  println(n, " has as 3nd friend ", a)end end)
+# only 6 is very perfect below 100001
+@time( @assert [[n,amico(n)]  for n in 1:1001 if amico(n)!=0] ==[[1,1],[6,6],[28,28],[220,284],[284,220],[496,496]])
+@time( @assert [[n,amico(n,3)]  for n in 1:1001 if amico(n,3)!=0]==[[1,1],[6,6],[28,28],[496,496]])
+@time( @assert [[n,amico(n,4)]  for n in 1:1001 if amico(n,4)!=0]==[[1,1],[6,6],[28,28],[220,284],[284,220],[496,496]])
+@time( [[n,amico(n,5)]  for n in 1:1001 if amico(n,5)!=0])
+@time( [[n,amico(n,6)]  for n in 1:1001 if amico(n,6)!=0])
+@time( [[n,amico(n,7)]  for n in 1:1001 if amico(n,7)!=0])
+@time( [[n,amico(n,8)]  for n in 1:1001 if amico(n,8)!=0])
+
+@time( [[n,moltoamico(n,1)]  for n in 1:1001 if moltoamico(n,1)!=0])
+@time( [[n,moltoamico(n,2)]  for n in 1:1001 if moltoamico(n,2)!=0])
+@time( [[n,moltoamico(n,3)]  for n in 1:1001 if moltoamico(n,3)!=0])
+@time( [[n,moltoamico(n,4)]  for n in 1:1001 if moltoamico(n,4)!=0])
+
+@time( [[n,moltoamico(n,5)]  for n in 1:1001 if moltoamico(n,5)!=0])
+@time( [[n,moltoamico(n,6)]  for n in 1:1001 if moltoamico(n,6)!=0])
+@time( [[n,moltoamico(n,7)]  for n in 1:1001 if moltoamico(n,7)!=0])
+@time( [[n,moltoamico(n,8)]  for n in 1:1001 if moltoamico(n,8)!=0])
+
+
 
 
 ## plot some prime things
 length(primes)
 
-list= begin n=10000; [isPerfect(i,0) for i in 1:n] end
-list= begin n=10000; [isFatPerfect(i,0) for i in 1:n] end
-count( x-> x>0, list)
+list= begin n=10000; [[i,isPerfect(i,0)] for i in 1:n] end
+# or list= begin n=10000; [i,isVeryPerfect(i,0)] for i in 1:n] end
+count( x[:,2].>0, list)
+
 count(x-> x<0, list)
 count(x-> x==0, list)
 using Plots; gr()
